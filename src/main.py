330d7e11e4effe9b832c22e26b8dc43cb110e945
beniman8 <benik@live.ca>
2024-06-18 23:34:30 -0400

    how to display and add things on game surface

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import pygame
+from os.path import join
+from random import randint
 
 # general setup
 pygame.init()
@@ -7,6 +9,16 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 running = True
 pygame.display.set_caption('Space Shooter')
 
+# plain surface
+surf = pygame.Surface((100,200))
+surf.fill('orange')
+x = 100
+
+# importing image
+player_surf = pygame.image.load(join('images','player.png')).convert_alpha()
+star_surf = pygame.image.load(join('images','star.png')).convert_alpha()
+star_positions = [(randint(10,WINDOW_WIDTH),randint(10,WINDOW_HEIGHT)) for i in range(20)]
+AMOUNT_OF_STARS=20
 
 while running:
     
@@ -17,8 +29,13 @@ while running:
     
     # draw the game
     # fill window with red color
-    
-    display_surface.fill('blue')
+    x +=0.1
+    display_surface.fill('darkgrey')
+
+    for pos in star_positions:   
+        display_surface.blit(star_surf,pos)
+        
+    display_surface.blit(player_surf,(x,150))
 
     pygame.display.update()
     
