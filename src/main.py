8566bbaabd8ab4f033841c4194edbde1f05e4288
beniman8 <benik@live.ca>
2024-06-21 01:15:35 -0400

    keyboard input

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ x = 100
 # importing image
 player_surf = pygame.image.load(join('images','player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center=(WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
-player_direction = pygame.math.Vector2(2,-2)
+player_direction = pygame.math.Vector2()
 player_speed = 300
 
 star_surf = pygame.image.load(join('images','star.png')).convert_alpha()
@@ -40,6 +40,30 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # keyboard and mouse input via event loop
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #     print(1)
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
+        
+        
+    #input
+    #pygame.mouse.get_pos()
+    pygame.key.set_repeat()
+    keys  = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    
+    recent_keys = pygame.key.get_just_pressed()
+    if recent_keys[pygame.K_SPACE]:
+        print('laser')
+    # if keys[pygame.K_RIGHT]:
+    #     player_direction.x = 1
+    # else:
+    #     player_direction.x = 0
+    player_rect.center += player_direction * player_speed * dt
+    
     
     # draw the game
     # fill window with red color
@@ -52,11 +76,11 @@ while running:
     display_surface.blit(laser_surf,laser_rect)
     
     #player movement
-    player_rect.center += player_direction * player_speed * dt
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *=-1
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction.x *= -1
+    # player_rect.center += player_direction * player_speed * dt
+    # if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
+    #     player_direction.y *=-1
+    # if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+    #     player_direction.x *= -1
         
         
     
